56c5225ec00e56ed16d398defe58d7d8edb3a404
ChristianViracocha44 <sistemas.ti4@sedemi.com>
2024-06-19 12:27:32 -0500

    Cargar el Home Principal
    Carga del componente Home Principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { HomePrincipalComponent } from './home-principal/home-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
-    HomeComponent
+    LoginComponent,
+    HomeComponent,
+    HomePrincipalComponent
   ],
   imports: [
     BrowserModule,
